00b6ad83c51ec570258728c5d2dc95f0435857b3
Maël <mael.nogues@gmail.com>
2016-10-22 12:28:47 +0200

    Correction du workspace d'aco

---
+++ aco/fr.istic.m1.aco.miniediteur.v1/src/event/Subject.java
@@ -0,0 +1,22 @@
+/*
+ * The GNU General Public License does not permit incorporating your program
+ * into proprietary programs. If your program is a subroutine library, you may
+ * consider it more useful to permit linking proprietary applications with the
+ * library. If this is what you want to do, use the GNU Lesser General Public
+ * License instead of this License. But first, please read
+ * <http://www.gnu.org/philosophy/why-not-lgpl.html>.
+ * @author Maël Nogues mael.nogues@etudiant.univ-rennes.fr
+ * @author Mathieu Grandmontagne mathieu.grandmontagne@etudiant.univ-rennes.fr
+ */
+package event;
+
+/**
+ * The Interface Subject.
+ */
+public interface Subject {
+
+	/** The event dispatcher. */
+	EventDispatcher ed = EventDispatcher.getInstance();
+
+	int[] selection = { 0, 0 };
+}
