ec5d49468a63d8d7b728955b896788fc9bbd6be9
mathieu-GM <grandmontagne.math@gmail.com>
2016-10-22 23:18:45 +0200

    update tp3 secu

--- secu/TP3/exo1.py
+++ secu/TP3/exo1.py
@@ -1,45 +1,56 @@
-from random import randint
+#!/usr/bin/env python
+from random import randrange
+
 from random import getrandbits
-
-def lpowmod(x, y, n):
-    result = 1
-    while y > 0:
-        if y&1 > 0:
-            result = (result * x) %n
+# renvoi (x^y)%n
+def lpowmod(x, y, z):
+    
+    number = 1
+    while y:
+        if y & 1:
+            number = number * x % z
         y >>= 1
-        x = (x * x) %n
-    return result
+        x = x * x % z
+    return number
 
 
-
-
+#true si a^p =1 mod p
 def fermat(a, p):
-    return pow(a, p-1, p) == 1
-
-
-def prim(p) :
-	if p not in [0,2,3,5,7] :
+	if p == 2:
+		return True
+	if not p & 1:
+		return False
+	return lpowmod(a, p-1, p) == 1
+    
+#~ print fermat(7,19)
+# renvoi true si 
+def prim(n) :
+	if n not in [0,1,2,3,5,7] :
 		for  i in [2, 3, 5, 7] :
-			r = fermat(p, i)
+			r = fermat(n, i)
 			if r != 1 :
-				r = False
-	return r
+				return False
+		return True
+	
+			
+		
 
-#print prim(97)
-
+#~ print prim(7)
+#revoi un nombre premier de n bit
 def randompri(n) :
 	a = getrandbits(n)
-	while prim(a) == False :
+	while prim(a) == False or a ==1 or a== 0 or a%2==0:
 		a = getrandbits(n)
 	return a
+#~ print randompri(15)
 
 #print randompri(77)
-#def epgcd(a, b):
-    #if a == 0:
-        #return (b, 0, 1)
-    #else:
-        #g, y, x = epgcd(b % a, a)
-        #return (g, x - (b // a) * y, y)
+def epgcd(a, b):
+    if a == 0:
+        return (b, 0, 1)
+    else:
+        g, y, x = epgcd(b % a, a)
+        return (g, x - (b // a) * y, y)
 
 #def xpgcd(b, n):
     #x0, x1, y0, y1 = 1, 0, 0, 1
@@ -56,47 +67,103 @@ def randompri(n) :
 
 
 #print invmod(5,97)
+def pgcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
 
-def inverse_modulaire(a, b) :
-	r = a
-	rp = b
-	u = 1
-	v = 0
-	up = 0
-	vp= 1
+def inverse_modulaire(a, phi) :
+	d = 0
+	x1 = 0
+	x2 = 1
+	y1 = 1
+	temp_phi = phi
 
-	while rp != 0 :
-		q = r / rp
-		rs = r
-		us = u
-		vs = v
-		r = rp
-		u = up
-		v = vp
-		rp = rs - q * rp
-		up = us - q * u
-		vp = vs - q * vp
+	while a > 0:
+		temp1 = temp_phi/a
+		temp2 = temp_phi - temp1 * a
+		temp_phi = a
+		a = temp2
+		
+		x = x2- temp1* x1
+		y = d - temp1 * y1
+		
+		x2 = x1
+		x1 = x
+		d = y1
+		y1 = y
 
-	return u
+	if temp_phi == 1:
+		return d + phi
 
-print inverse_modulaire(5, 77)
 
-def keygenerator() :
-	p = q = 0
+#print inverse_modulaire(,)
+
+def keygenerator(taille) :
+	p = q = 3
 	while p == q :
-		p = randompri(1024)
-		q = randompri(1024)
+		p = randompri(taille)
+		q = randompri(taille)
 	n = p *q
 	phiden =  (p - 1) * (q - 1)
-	e = phiden
-	while inverse_modulaire(e,phiden) != 1 and e > 3:
-		e = randint(3, e - 1)
-        print e
+#	print (phiden, n-phiden)
+	e = randrange(1, phiden)
+	while  pgcd(e,phiden) != 1:
+		e = randrange(1, phiden)
 	d = inverse_modulaire(e, phiden)
-	while d < 0 :
+	
+	while d <= 0 or d==e :
 		d = d + phiden
-	return (e, n), (p, q, d)
+	return (e, n), (d, n)
 
-print keygenerator()
+#public, private= keygenerator(512)
 
-# afficher les 100 nombre permier
+
+message = 'abc'
+
+def encrypt(pk, plaintext):
+    #la cle publique
+    e, n = pk
+    #pour chaque caratere on aplique c^e mod n
+    cipher = [lpowmod(ord(char),e,n) for char in plaintext]
+ #   print cipher
+    return cipher
+
+def decrypt(pk, ciphertext):
+   #la cle privee
+	d, n = pk
+   #pour chaque caractere on aplique c^d mod n
+	plain = [lpowmod(char,d, n) for char in ciphertext]
+#	print plain
+	return plain
+	
+if __name__ == '__main__':
+	#print "RSA Encrypter/ Decrypter"
+	#print "Your public key is ", public ," and your private key is ", private
+	#message = raw_input("Enter a message to encrypt with your private key: ")
+	public, private= keygenerator(3)
+	print public , private
+	encrypted_msg = encrypt(public, message)
+	print ("message chiffre")
+	print encrypted_msg
+	print "message dechifre"
+	decrypt_msg= decrypt(private, encrypted_msg)
+	print decrypt_msg
+	if encrypted_msg==decrypt_msg :
+			print "chifrement/dechifrement focntionnel"
+	else :
+		print "erreur"
+	
+	
+	
+	#~ while encrypted_msg!=decrypt_msg :
+		#~ public, private= keygenerator(20)
+		#~ i= i+1
+		#~ print i
+		#~ encrypted_msg = encrypt(public, message)
+		#~ decrypt_msg= decrypt(private, encrypted_msg)	
+		#~ if encrypted_msg==decrypt_msg :
+			#~ print "chifrement/dechifrement focntionnel"
+			
+		
+
