3a95eb0ffdfbdaa957c1f69d9f92bda210319df7
mathieu-GM <grandmontagne.math@gmail.com>
2016-10-23 16:57:06 +0200

    fin tp3 secu

--- secu/TP3/ex2.py
+++ secu/TP3/ex2.py
@@ -1,6 +1,36 @@
+#!/usr/bin/env python
 from sys import argv
 from time import time
 
+#renvoi (x^y)%n en utilisant l'exponentiation modulaire O(log(n))
+def lpowmod(x, y, z): 
+    a = 1
+    while y > 0:
+        if y % 2 == 1:
+            a = (a * x) % z
+        x = (x * x) % z
+        y //= 2
+    return a
+
+
+#true si a^p =1 mod p 
+def fermat(a, p):
+	if p == 2:
+		return True
+	if not p & 1:
+		return False
+	return lpowmod(a, p-1, p) == 1
+    
+#~ print fermat(7,19)
+# renvoi true si 2^p-1 = 3^p-1 = 5^p-1 = 7^p-1 = 1 mod p
+def prim(n) :
+	if n not in [0,1,2,3,5,7] :#on exclu ces valeur pour n
+		for  i in [2, 3, 5, 7] :# pour chaque valeur on regarde si fermat renvoi true
+			r = fermat(i, n)
+			if r != 1 :
+				return False
+		return True
+		
 def prime(i, primes): #sieve of erathostenes
 	for prime in primes: #goes through all primes in the list at the moment
 		if not (i == prime or i % prime): #checks if we can cross out the number i
@@ -21,7 +51,22 @@ def historic(n):
 if __name__ == "__main__":
 	if len(argv) > 1 :
 		start = time()
-		print sorted(list(historic(int(argv[1])))) #prints the given number of prime numbers
+		length = int(argv[1])
+		print sorted(list(historic(length))) #prints the given number of prime numbers
 		print 'Time in seconds: ' + str(time() - start) #prints the execution time in seconds
+		true = 0
+		false = 0
+		for i  in historic(length) :
+			if i in [2,3,5,7] :
+				true = true +1
+			elif prim(i)==True :
+				true= true +1
+			else :
+				false = false +1
+		if true == length:
+			print "for PGP primality test all number are prime"
+		else :
+			print "for PGP primality test all number are not prime"
+					 
 	else :
 		print 'Parameter needed : number of primes to display'
