720b81d60b95c4c324b0785af61a38ddef2bcaf8
mael <mael.nogues@gmail.com>
2017-01-26 17:48:49 +0100

    meps

--- meps/monteSphere.py
+++ meps/monteSphere.py
@@ -2,12 +2,12 @@ import random
 import math
 
 nbrand = int(input('Nombre d\'essai : '))
-lamb = int(input('Lambda : '))
+radius = int(input('Radius : '))
+center = vector(radius, radius, radius)
 
 x = 0
-result = 0
 while x < nbrand:
-    result += - (1/lamb) * math.log(random.uniform(0,1))
+    point = (random.uniform(0,1), random.uniform(0,1), random.uniform(0,1))
     x+=1
 e = result / nbrand
 print ('E[X] = ', e)
