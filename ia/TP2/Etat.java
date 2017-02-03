29adf519c5869036551da00520f26f9419312ecd
mael <mael.nogues@gmail.com>
2017-02-03 17:06:55 +0100

    modif état

--- ia/TP2/Etat.java
+++ ia/TP2/Etat.java
@@ -1,358 +1,396 @@
+
 /*
  * TP 2 - IA: Taquin
- * 
  * @author Tassadit BOUADI.
  */
 
 import java.util.Vector;
 
-public class Etat implements Comparable<Etat>{
-	public static final int[][] ETAT_FINAL1 = {
-		{1,2,3},
-		{8,0,4},
-		{7,6,5}};
-	public static final int[][] ETAT_FINAL2 = {
-		{1,2,3},
-		{4,5,6},
-		{7,8,0}};
-	
-	private Etat _pere; //l'état précédent 
-	private int[][] _taquin; //la configuration de jeu
-	private int _xVide; //les coordonnées de la case vide
-	private int _yVide;
-	private Vector<Deplacement> _coups; //la suite de déplacements de la case vide
-	private int _nbCoups;
-	private int _valG; //les valeurs des fonctions associées à la configuration
-	private int _valF;
-	
-	
+public class Etat implements Comparable<Etat> {
+	public static final int[][]	ETAT_FINAL1	= { { 1, 2, 3 }, { 8, 0, 4 }, { 7, 6, 5 } };
+	public static final int[][]	ETAT_FINAL2	= { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 0 } };
+
+	private Etat				_pere;		// l'ï¿½tat prï¿½cï¿½dent
+	private int[][]				_taquin;	// la configuration de jeu
+	private int					_xVide;		// les coordonnï¿½es de la case vide
+	private int					_yVide;
+	private Vector<Deplacement>	_coups;		// la suite de dï¿½placements de la
+											// case vide
+	private int					_nbCoups;
+	private int					_valG;		// les valeurs des fonctions
+											// associï¿½es ï¿½ la configuration
+	private int					_valF;
+
+	/**
+	 * Constructeur qui recopie l'ï¿½tat e dans l'ï¿½tat courant.
+	 * 
+	 * @param e
+	 *            : l'ï¿½tat ï¿½ cloner.
+	 */
+	public Etat(Etat e) {
+		_pere = e._pere;
+		_taquin = new int[3][3];
+		for (int i = 0; i < 3; i++)
+			for (int j = 0; j < 3; j++)
+				_taquin[i][j] = e._taquin[i][j];
+		_xVide = e._xVide;
+		_yVide = e._yVide;
+		_coups = new Vector<>();
+		for (int i = 0; i < e._nbCoups; i++)
+			_coups.add(e._coups.get(i));
+		_nbCoups = e._nbCoups;
+		_valG = e._valG;
+		_valF = e._valF;
+	}
+
 	/**
 	 * Constructeur.
-	 * @param tab : le tableau décrivant la configuration de jeu.
+	 * 
+	 * @param tab
+	 *            : le tableau dï¿½crivant la configuration de jeu.
 	 */
-	public Etat(int[][] tab, FonctionHeuristique heurist){
+	public Etat(int[][] tab) {
 		_pere = null;
 		_taquin = new int[3][3];
-		for(int i=0; i<3; i++){
-			for(int j=0; j<3; j++){
+		for (int i = 0; i < 3; i++)
+			for (int j = 0; j < 3; j++) {
 				_taquin[i][j] = tab[i][j];
-				if(_taquin[i][j] == 0){
+				if (_taquin[i][j] == 0) {
 					_xVide = i;
 					_yVide = j;
 				}
 			}
-		}
-		_coups = new Vector<Deplacement>();
+		_coups = new Vector<>();
+		_nbCoups = 0;
+		_valG = 0;
+		_valF = _valG;
+	}
+
+	/**
+	 * Constructeur.
+	 * 
+	 * @param tab
+	 *            : le tableau dï¿½crivant la configuration de jeu.
+	 */
+	public Etat(int[][] tab, FonctionHeuristique heurist) {
+		_pere = null;
+		_taquin = new int[3][3];
+		for (int i = 0; i < 3; i++)
+			for (int j = 0; j < 3; j++) {
+				_taquin[i][j] = tab[i][j];
+				if (_taquin[i][j] == 0) {
+					_xVide = i;
+					_yVide = j;
+				}
+			}
+		_coups = new Vector<>();
 		_nbCoups = 0;
 		_valG = 0;
 		int valH = heurist.heuristique(this);
 		_valF = _valG + valH;
 	}
-	
-	
-	/**
-	 * Constructeur.
-	 * @param tab : le tableau décrivant la configuration de jeu.
-	 */
-	public Etat(int[][] tab){
-		_pere = null;
-		_taquin = new int[3][3];
-		for(int i=0; i<3; i++){
-			for(int j=0; j<3; j++){
-				_taquin[i][j] = tab[i][j];
-				if(_taquin[i][j] == 0){
-					_xVide = i;
-					_yVide = j;
-				}
-			}
-		}
-		_coups = new Vector<Deplacement>();
-		_nbCoups = 0;
-		_valG = 0;
-		_valF = _valG;
-	}
-	
-	
-	/**
-	 * Constructeur qui recopie l'état e dans l'état courant.
-	 * @param e : l'état à cloner.
-	 */
-	public Etat(Etat e){
-		_pere = e._pere;
-		_taquin = new int[3][3];
-		for(int i=0; i<3; i++){
-			for(int j=0; j<3; j++){
-				_taquin[i][j] = e._taquin[i][j];
-			}
-		}
-		_xVide = e._xVide;
-		_yVide = e._yVide;
-		_coups = new Vector<Deplacement>();
-		for(int i=0; i<e._nbCoups; i++){
-			_coups.add(e._coups.get(i));
-		}
-		_nbCoups = e._nbCoups;
-		_valG = e._valG;
-		_valF = e._valF;
-	}
-	
-	
-	
-	/**
-	 * Méthode qui donne le père de l'état courant.
-	 * @return le père de l'état courant.
-	 */
-	public Etat getPere(){
-		return _pere;
-	}
-	
-	
-	/**
-	 * Méthode qui donne la valeur de la fonction d'évaluation g.
-	 * @return la valeur de la fonction g.
-	 */
-	public int getValG(){
-		return _valG;
-	}
-	
-	
-	/**
-	 * Méthode qui donne la valeur de la fonction d'évaluation f.
-	 * @return la valeur de la fonction f.
-	 */
-	public int getValF(){
-		return _valF;
-	}
-	
-	
-	/**
-	 * Méthode qui donne la valeur de la case de coordonnées données.
-	 * @param i : l'abscisse de la case.
-	 * @param j : l'ordonnée de la case.
-	 * @return la valeur de la case.
-	 */
-	public int getVal(int i, int j){
-		int val = -1;
-		
-		if((i>=0 && i<3) && (j>=0 && j<3)){
-			val = _taquin[i][j];
-		}
-		
-		return val;
-	}
-	
-	
-	/**
-	 * Méthode qui donne l'abscisse du nombre n.
-	 * @param n : le nombre considéré.
-	 * @return l'abscisse de n.
-	 */
-	public int getX(int n){
-		int x = -1;
-		
-		for(int i=0; i<3 && x==-1; i++){
-			for(int j=0; j<3&& x==-1; j++){
-				if(_taquin[i][j] == n){
-					x = i;
-				}
-			}
-		}
-		
-		return x;
-	}
-	
-	
-	/**
-	 * Méthode qui donne ordonnée du nombre n.
-	 * @param n : le nombre considéré.
-	 * @return ordonnée de n.
-	 */
-	public int getY(int n){
-		int y = -1;
-		
-		for(int i=0; i<3 && y==-1; i++){
-			for(int j=0; j<3&& y==-1; j++){
-				if(_taquin[i][j] == n){
-					y = j;
-				}
-			}
-		}
-		
-		return y;
-	}
-	
-	
-	/**
-	 * Méthode qui dit si l'état courant est un état final.
-	 * @return vrai si l'état courant correspond à un des 2 états finaux.
-	 */
-	public boolean estFinal(){
-		Etat eFinal1 = new Etat(ETAT_FINAL1);
-		Etat eFinal2 = new Etat(ETAT_FINAL2);
-		
-		return (equals(eFinal1) || equals(eFinal2));
-	}
-	
-	
-	
-	/**
-	 * Méthode qui donne les états successeurs de l'état courant,
-	 * calculés selon l'emplacement de la case vide et en utilisant
-	 * la fonction heuristique donnée.
-	 * @return les états successeurs.
-	 */
-	public Vector<Etat> getSuccesseurs(FonctionHeuristique heurist){
-		Deplacement d;
-		Vector<Etat> succs = new Vector<Etat>();
-		
-		Deplacement[] depl = Deplacement.values(); //l'ensemble des déplacements
-		//à compléter...
-		
-		return succs;
-	}
-	
-	
-	/**
-	 * Méthode qui dit s'il est possible de déplacer la case vide,
-	 * dans la direction donnée par d.
-	 * @param d : la direction pour déplacer la case vide.
-	 * @return vrai si la case vide peut être déplacée dans la direction d.
-	 */
-	private boolean deplacementPossible(Deplacement d){
-		boolean depl = true;
-		
-		//à compléter...
-		
-		return depl;
-	}
-	
-	
-	/**
-	 * Méthode qui étend l'état courant, en déplaçant la case vide
-	 * dans la direction donnée par d.
-	 * @param d : la direction pour déplacer la case vide.
-	 * @return l'état créé à partir de l'état courant
-	 */
-	private Etat etendEtat(Deplacement d, FonctionHeuristique heurist){
-		Etat etat = new Etat(this);
-		etat._pere = this;
-		
-		//déplacement de la case vide
-		//à compléter...
-		
-		//mise à jour des fonctions d'évaluation 
-		//à compléter...
-		
-		return etat;
-	}
-	
-	
-	
-	/**
-	 * Méthode qui affiche la séquence de configurations.
-	 */
-	public void afficherParcours(){
-		if(_pere != null){
-			_pere.afficherParcours();
-		}
-		System.out.println(this);
-	}
-	
 
 	/**
-	 * Méthode qui donne la représentation de l'état, sous forme de chaîne.
-	 * @return la chaîne représentant l'état.
+	 * Mï¿½thode qui affiche la sï¿½quence de configurations.
 	 */
-	public String toString(){
+	public void afficherParcours() {
+		if (_pere != null)
+			_pere.afficherParcours();
+		System.out.println(this);
+	}
+
+	/**
+	 * Mï¿½thode de l'interface Comparable, ï¿½ implï¿½menter.
+	 * 
+	 * @param e
+	 *            : l'ï¿½tat avec lequel comparer l'ï¿½tat courant.
+	 * @return -1, 0 ou 1 selon que l'ï¿½tat e est infï¿½rieur, ï¿½gal ou supï¿½rieur ï¿½
+	 *         l'ï¿½tat courant, respectivement.
+	 */
+	@Override
+	public int compareTo(Etat e) {
+		int res = -1;
+
+		// classement dans l'ordre croissant de f
+		// puis dans l'ordre dï¿½croissant de g
+		if (e._valF < _valF)
+			res = -1;
+		else if (e._valF > _valF)
+			res = 1;
+		else if (e._valG > _valG)
+			res = -1;
+		else if (e._valG < _valG)
+			res = 1;
+		else
+			res = 0;
+
+		return res;
+	}
+
+	/**
+	 * Mï¿½thode qui dit s'il est possible de dï¿½placer la case vide, dans la
+	 * direction donnï¿½e par d.
+	 * 
+	 * @param d
+	 *            : la direction pour dï¿½placer la case vide.
+	 * @return vrai si la case vide peut ï¿½tre dï¿½placï¿½e dans la direction d.
+	 */
+	private boolean deplacementPossible(Deplacement d) {
+		boolean depl = true;
+
+		switch (d) {
+			case haut:
+				if (_yVide == 3)
+					depl = !depl;
+				break;
+			case bas:
+				if (_yVide == 0)
+					depl = !depl;
+				break;
+			case gauche:
+				if (_xVide == 0)
+					depl = !depl;
+				break;
+			case droite:
+				if (_xVide == 3)
+					depl = !depl;
+				break;
+		}
+
+		return depl;
+	}
+
+	/**
+	 * Mï¿½thode qui dit si 2 ï¿½tats sont ï¿½gaux, au niveau des configurations.
+	 * 
+	 * @param e
+	 *            : l'ï¿½tat avec lequel comparer l'ï¿½tat courant.
+	 * @return vrai si les ï¿½tats correspondent ï¿½ la mï¿½me configuration.
+	 */
+	public boolean equals(Etat e) {
+		boolean eq = true;
+
+		for (int i = 0; i < 3 && eq; i++)
+			for (int j = 0; j < 3 && eq; j++)
+				eq = _taquin[i][j] == e._taquin[i][j];
+
+		return eq;
+	}
+
+	/**
+	 * Mï¿½thode qui dit si l'ï¿½tat courant est un ï¿½tat final.
+	 * 
+	 * @return vrai si l'ï¿½tat courant correspond ï¿½ un des 2 ï¿½tats finaux.
+	 */
+	public boolean estFinal() {
+		Etat eFinal1 = new Etat(ETAT_FINAL1);
+		Etat eFinal2 = new Etat(ETAT_FINAL2);
+
+		return equals(eFinal1) || equals(eFinal2);
+	}
+
+	/**
+	 * Mï¿½thode qui ï¿½tend l'ï¿½tat courant, en dï¿½plaï¿½ant la case vide dans la
+	 * direction donnï¿½e par d.
+	 * 
+	 * @param d
+	 *            : la direction pour dï¿½placer la case vide.
+	 * @return l'ï¿½tat crï¿½ï¿½ ï¿½ partir de l'ï¿½tat courant
+	 */
+	private Etat etendEtat(Deplacement d, FonctionHeuristique heurist) {
+		Etat etat = new Etat(this);
+		etat._pere = this;
+		int tmp;
+
+		// dï¿½placement de la case vide
+
+		if (deplacementPossible(d)) {
+			switch (d) {
+				case haut:
+					etat._yVide++;
+					tmp = etat._taquin[etat._xVide][etat._yVide];
+					etat._taquin[etat._xVide][etat._yVide] = 0;
+					etat._taquin[etat._xVide][etat._yVide - 1] = tmp;
+					break;
+				case bas:
+					etat._yVide--;
+					tmp = etat._taquin[etat._xVide][etat._yVide];
+					etat._taquin[etat._xVide][etat._yVide] = 0;
+					etat._taquin[etat._xVide][etat._yVide + 1] = tmp;
+					break;
+				case gauche:
+					etat._xVide--;
+					tmp = etat._taquin[etat._xVide][etat._yVide];
+					etat._taquin[etat._xVide][etat._yVide] = 0;
+					etat._taquin[etat._xVide + 1][etat._yVide] = tmp;
+					break;
+				case droite:
+					etat._xVide++;
+					tmp = etat._taquin[etat._xVide][etat._yVide];
+					etat._taquin[etat._xVide][etat._yVide] = 0;
+					etat._taquin[etat._xVide - 1][etat._yVide] = tmp;
+					break;
+			}
+			etat._coups.addElement(d);
+			etat._nbCoups++;
+			etat._valG++;
+			etat._valF = etat._valG + heurist.heuristique(etat);
+		}
+		else
+			return null;
+
+		// mise ï¿½ jour des fonctions d'ï¿½valuation
+		// ï¿½ complï¿½ter...
+
+		return etat;
+	}
+
+	/**
+	 * Mï¿½thode qui donne le pï¿½re de l'ï¿½tat courant.
+	 * 
+	 * @return le pï¿½re de l'ï¿½tat courant.
+	 */
+	public Etat getPere() {
+		return _pere;
+	}
+
+	/**
+	 * Mï¿½thode qui donne les ï¿½tats successeurs de l'ï¿½tat courant, calculï¿½s selon
+	 * l'emplacement de la case vide et en utilisant la fonction heuristique
+	 * donnï¿½e.
+	 * 
+	 * @return les ï¿½tats successeurs.
+	 */
+	public Vector<Etat> getSuccesseurs(FonctionHeuristique heurist) {
+		Vector<Etat> succs = new Vector<>();
+		Etat e = null;
+
+		for (Deplacement d : Deplacement.values())
+			if ((e = etendEtat(d, heurist)) != null)
+				succs.add(e);
+
+		return succs;
+	}
+
+	/**
+	 * Mï¿½thode qui donne la valeur de la case de coordonnï¿½es donnï¿½es.
+	 * 
+	 * @param i
+	 *            : l'abscisse de la case.
+	 * @param j
+	 *            : l'ordonnï¿½e de la case.
+	 * @return la valeur de la case.
+	 */
+	public int getVal(int i, int j) {
+		int val = -1;
+
+		if (i >= 0 && i < 3 && j >= 0 && j < 3)
+			val = _taquin[i][j];
+
+		return val;
+	}
+
+	/**
+	 * Mï¿½thode qui donne la valeur de la fonction d'ï¿½valuation f.
+	 * 
+	 * @return la valeur de la fonction f.
+	 */
+	public int getValF() {
+		return _valF;
+	}
+
+	/**
+	 * Mï¿½thode qui donne la valeur de la fonction d'ï¿½valuation g.
+	 * 
+	 * @return la valeur de la fonction g.
+	 */
+	public int getValG() {
+		return _valG;
+	}
+
+	/**
+	 * Mï¿½thode qui donne l'abscisse du nombre n.
+	 * 
+	 * @param n
+	 *            : le nombre considï¿½rï¿½.
+	 * @return l'abscisse de n.
+	 */
+	public int getX(int n) {
+		int x = -1;
+
+		for (int i = 0; i < 3 && x == -1; i++)
+			for (int j = 0; j < 3 && x == -1; j++)
+				if (_taquin[i][j] == n)
+					x = i;
+
+		return x;
+	}
+
+	/**
+	 * Mï¿½thode qui donne ordonnï¿½e du nombre n.
+	 * 
+	 * @param n
+	 *            : le nombre considï¿½rï¿½.
+	 * @return ordonnï¿½e de n.
+	 */
+	public int getY(int n) {
+		int y = -1;
+
+		for (int i = 0; i < 3 && y == -1; i++)
+			for (int j = 0; j < 3 && y == -1; j++)
+				if (_taquin[i][j] == n)
+					y = j;
+
+		return y;
+	}
+
+	/**
+	 * Mï¿½thode qui donne la reprï¿½sentation de l'ï¿½tat, sous forme de chaï¿½ne.
+	 * 
+	 * @return la chaï¿½ne reprï¿½sentant l'ï¿½tat.
+	 */
+	@Override
+	public String toString() {
 		String str = "";
-		
-		//la configuration de jeu
+
+		// la configuration de jeu
 		str += "-------\n";
-		for(int i=0; i<3; i++){
+		for (int i = 0; i < 3; i++) {
 			str += "|";
-			for(int j=0; j<3; j++){
-				if(j > 0) str += " ";
+			for (int j = 0; j < 3; j++) {
+				if (j > 0)
+					str += " ";
 				str += _taquin[i][j];
 			}
 			str += "|\n";
 		}
 		str += "-------\n";
-		
-		//les valeurs des fonctions
+
+		// les valeurs des fonctions
 		str += "f = " + _valF + " (g = " + _valG + ")\n";
-		
-		//les déplacements de la case vide
-		str += _nbCoups + " déplacements : ";
-		for(int i = 0; i < _nbCoups; i++){
-			if(i > 0) str += " - ";
-			switch(_coups.get(i)){
-			case haut:
-				str += "haut";
-				break;
-			case bas:
-				str += "bas";
-				break;
-			case gauche:
-				str += "gauche";
-				break;
-			case droite:
-				str += "droite";
-				break;
+
+		// les dï¿½placements de la case vide
+		str += _nbCoups + " dï¿½placements : ";
+		for (int i = 0; i < _nbCoups; i++) {
+			if (i > 0)
+				str += " - ";
+			switch (_coups.get(i)) {
+				case haut:
+					str += "haut";
+					break;
+				case bas:
+					str += "bas";
+					break;
+				case gauche:
+					str += "gauche";
+					break;
+				case droite:
+					str += "droite";
+					break;
 			}
-		}//for i
-		
+		} // for i
+
 		return str;
 	}
-	
-	
-	/**
-	 * Méthode qui dit si 2 états sont égaux, au niveau des configurations.
-	 * @param e : l'état avec lequel comparer l'état courant.
-	 * @return vrai si les états correspondent à la même configuration.
-	 */
-	public boolean equals(Etat e){
-		boolean eq = true;
-		
-		for(int i=0; i<3 && eq; i++){
-			for(int j=0; j<3 && eq; j++){
-				eq = (_taquin[i][j] == e._taquin[i][j]);
-			}
-		}
-		
-		return eq;
-	}
-	
-	
-	/**
-	 * Méthode de l'interface Comparable, à implémenter.
-	 * @param e : l'état avec lequel comparer l'état courant.
-	 * @return -1, 0 ou 1 selon que l'état e est inférieur, égal
-	 * ou supérieur à l'état courant, respectivement.
-	 */
-	public int compareTo(Etat e){
-		int res = -1;
-		
-		//classement dans l'ordre croissant de f 
-		//puis dans l'ordre décroissant de g
-		if(e._valF < _valF){
-			res = -1;
-		}
-		else if(e._valF > _valF){
-			res = 1;
-		}
-		else{
-			if(e._valG > _valG){
-				res = -1;
-			}
-			else if(e._valG < _valG){
-				res = 1;
-			}
-			else{
-				res = 0;
-			}
-		}
-		
-		return res;
-	}
-	
+
 }
