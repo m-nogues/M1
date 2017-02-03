3fe4c6a934b7628c2a1eec9793c9d6c047f9510f
Mathieu Grandmontagne <14000806@e105m03.istic.univ-rennes1.fr>
2017-02-03 09:58:34 +0100

    ca avance trop vite

--- SGM/nachos/vm/pagefaultmanager.cc
+++ SGM/nachos/vm/pagefaultmanager.cc
@@ -3,8 +3,8 @@ Routines for the page fault managerPage Fault Manager
 */
 //
 //  Copyright (c) 1999-2000 INSA de Rennes.
-//  All rights reserved.  
-//  See copyright_insa.h for copyright notice and limitation 
+//  All rights reserved.
+//  See copyright_insa.h for copyright notice and limitation
 //  of liability and disclaimer of warranty provisions.
 //
 
@@ -23,8 +23,8 @@ PageFaultManager::~PageFaultManager() {
 }
 
 // ExceptionType PageFault(int virtualPage)
-/*! 	
-//	This method is called by the Memory Management Unit when there is a 
+/*!
+//	This method is called by the Memory Management Unit when there is a
 //      page fault. This method loads the page from :
 //      - read-only sections (text,rodata) $\Rightarrow$ executive
 //        file
@@ -38,14 +38,26 @@ PageFaultManager::~PageFaultManager() {
 //        size of the address space, and supposed to correspond to a
 //        page mapped to something [code/data/bss/...])
 //	\return the exception (generally the NO_EXCEPTION constant)
-*/  
-ExceptionType PageFaultManager::PageFault(int virtualPage) 
+*/
+ExceptionType PageFaultManager::PageFault(int virtualPage)
 {
+  #ifndef ETUDIANTS_TP
   printf("**** Warning: page fault manager is not implemented yet\n");
     exit(-1);
     return ((ExceptionType)0);
+  #endif
+  #ifdef ETUDIANTS_TP
+  Process *processus = g_current_thread->GetProcessOwner();
+  AddrSpace *addrspace = process->addrspace;
+  TranslationTable *tableTraduction = addrspace->translationTable;
+
+  //declaration
+
+//
+  if(tableTraduction->getBitIo(virtualPage)){
+    while ((tableTraduction->getBitIo(virtualPage))) {
+      g_current_thread->Yield();
+    }
+  }
+  #ifndef
 }
-
-
-
-
