a0159ae4b288fd9390cef6a625a0800d69eb54c3
Mathieu Grandmontagne <14000806@e003m03.istic.univ-rennes1.fr>
2017-03-24 18:05:49 +0100

    parser fini presque

--- ia/TP4_sujet/parser.py
+++ ia/TP4_sujet/parser.py
@@ -1,23 +1,71 @@
-
+import os
 
 #dataset
 # M {card,card...}
 # O {card, card,...}
 
-dico = {}
-inputfile = open('data/all_absolute-.txt')
-for line in inputfile:
+list = []
 
+listDico =[]
+listCardM = []
+listCardO = []
+i = 0
+inputfile = open('data/all_absolute+.txt')
+tempfile =  open('temp', 'w')
+for line in inputfile:
+    find = False
     mylist = line.split(" ")
     if mylist[1]=="Begin":
-        dicoCard = []
         game = mylist[0]
-        print(dico)
+        if len(listCardM) > 0 :
+            for i in sorted(listCardM) :
+                print(i, file = tempfile, end=' ')
+            print (file = tempfile)
+        if len(listCardO) > 0 :
+            for i in sorted(listCardO) :
+                print(i, file = tempfile, end=' ')
+            print (file = tempfile)
+
+        # listDico.append(sorted(listCardM))
+        # listDico.append(sorted(listCardO))
+        listCardM = []
+        listCardO = []
+
     else:
         player = mylist[1]
-        if mylist[0]==game :
-            dicoCard.append(mylist[1])
-        dico = {mylist[0]+player[:1] : dicoCard}
+        i+=1
+        card = mylist[1]
+        for id,v in enumerate(list):
+            if v == card[1:] :
+                find = True
+                index = id+1
+        if find == False :
+            list.append(card[1:])
+            index = len(list)
 
+        if player[:1]=="M" :
+            if index not in listCardM:
+                listCardM.append(index)
+        else :
+            if index not in listCardO:
+                listCardO.append(index)
 
+for i in sorted(listCardM) :
+    print(i, file = tempfile, end=' ')
+print (file = tempfile)
+
+for i in sorted(listCardO) :
+    print(i, file = tempfile, end=' ')
+print (file = tempfile)
+
+tempfile.close()
+# listDico.append(listCardM)
+# listDico.append(listCardO)
+
+# print(listDico)
+# print(i)
+print(list[1])
+# print(len(list))
+
+os.system("java -jar spmf.jar run LCM temp output.txt 10%")
 inputfile.close()
